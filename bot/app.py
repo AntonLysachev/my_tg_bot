d0a39669048cac10a3d1d2c653d254f4c2d000df
AntonLysachev <anton@lysachev.ru>
2024-03-03 15:32:06 +0500

    add telebot

--- bot/app.py
+++ bot/app.py
@@ -1,34 +1,54 @@
-import requests
 import os
 from dotenv import load_dotenv
-from flask import Flask, request
+from flask import Flask, request, abort
+import telebot
+import logging
 
 
 load_dotenv()
-token = os.getenv('TOKEN')
+TOKEN = os.getenv('TOKEN')
+URL = os.getenv('URL')
+logger = telebot.logger
+telebot.logger.setLevel(logging.INFO)
+bot = telebot.TeleBot(TOKEN)
+
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 
-def send_message(chat_id, text, token):
-    method = 'sendMessage'
-    url = f'https://api.telegram.org/bot{token}/{method}'
-    data = {'chat_id': chat_id, 'text': text}
-    requests.post(url, data=data)
+@app.route('/', methods=['GET', 'HEAD'])
+def index():
+    return ''
 
-@app.route('/', methods=['POST'])
-def process():
-    data = request.json
-    if data.get('message', False):
-        chat_id = request.json['message']['chat']['id']
-        name = request.json['message']['from']['first_name']
+
+@app.route(f'/{TOKEN}/', methods=['POST'])
+def webhook():
+    if request.headers.get('content-type') == 'application/json':
+        json_string = request.get_data().decode('utf-8')
+        update = telebot.types.Update.de_json(json_string)
+        bot.process_new_updates([update])
+        return ''
     else:
-        chat_id = request.json['edited_message']['chat']['id']
-        name = request.json['edited_message']['from']['first_name']
-    send_message(chat_id, f'Привет {name}!', token)
-    return {'ok': True}
+        abort(403)
+
+
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    print(message.chat.id)
+    bot.send_message(message.chat.id, f'Привет {message.chat.first_name}!')
+
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.send_message(message.chat.id, f'Привет {message.chat.first_name}!')
+
+
+
+bot.remove_webhook()
+bot.set_webhook(url=URL)
 
 debug_switch = os.getenv('DEBUG_SWITCH')
 
 if __name__ == '__main__':
-    app.run(debug=debug_switch)
+    app.run(host='0.0.0.0', debug=debug_switch)
