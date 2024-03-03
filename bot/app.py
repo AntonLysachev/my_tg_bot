be0b00c192b0f1fc9432b83c8f109e14224774c5
AntonLysachev <anton@lysachev.ru>
2024-03-03 18:10:00 +0500

    update

--- bot/app.py
+++ bot/app.py
@@ -13,16 +13,17 @@ app = Flask(__name__)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
+    bot.send_message(message.chat.id, f'Привет {message.chat.first_name}!')
 
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
-    bot.reply_to(message, message.text)
+    bot.send_message(message.chat.id, message.text)
 
 
 @app.route(f'/{TOKEN}/', methods=['POST'])
 def getMessage():
+    print('MESAGE')
     json_string = request.get_data().decode('utf-8')
     update = telebot.types.Update.de_json(json_string)
     bot.process_new_updates([update])
