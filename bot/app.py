df8f19dc65a5f467ace8a3e255bf03e586c1daff
AntonLysachev <anton@lysachev.ru>
2024-03-03 18:16:46 +0500

    delete coments

--- bot/app.py
+++ bot/app.py
@@ -23,7 +23,6 @@ def echo_message(message):
 
 @app.route(f'/{TOKEN}/', methods=['POST'])
 def getMessage():
-    print('MESAGE')
     json_string = request.get_data().decode('utf-8')
     update = telebot.types.Update.de_json(json_string)
     bot.process_new_updates([update])
@@ -39,60 +38,3 @@ def webhook():
 
 if __name__ == "__main__":
     app.run()
-
-
-
-
-# import os
-# from dotenv import load_dotenv
-# from flask import Flask, request, abort
-# import telebot
-# import logging
-# import json
-
-
-# load_dotenv()
-# TOKEN = os.getenv('TOKEN')
-# URL = os.getenv('URL')
-# logger = telebot.logger
-# telebot.logger.setLevel(logging.INFO)
-# bot = telebot.TeleBot(TOKEN)
-
-
-# app = Flask(__name__)
-# app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-
-
-# @app.route('/', methods=['GET', 'HEAD'])
-# def index():
-#     return ''
-
-
-# @app.route(f'/{TOKEN}/', methods=['POST'])
-# def webhook():
-#     if request.headers.get('content-type') == 'application/json':
-#         json_string = request.get_data().decode('utf-8')
-#         update = telebot.types.Update.de_json(json_string)
-#         bot.process_new_updates([update])
-#         return ''
-#     else:
-#         abort(403)
-
-
-# @bot.message_handler(commands=['help', 'start'])
-# def send_welcome(message):
-#     bot.send_message(message.chat.id, f'Привет {message.chat.first_name}!')
-
-
-# @bot.message_handler(func=lambda message: True)
-# def echo_all(message):
-#     bot.send_message(message.chat.id, f'Привет {message.chat.first_name}!')
-
-
-# bot.remove_webhook()
-# bot.set_webhook(url=URL)
-
-# debug_switch = os.getenv('DEBUG_SWITCH')
-
-# if __name__ == '__main__':
-#     app.run(debug=debug_switch)
