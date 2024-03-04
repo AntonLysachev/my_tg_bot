e024e9279f9c90879d44e75b383aef625e7269b4
AntonLysachev <anton@lysachev.ru>
2024-03-04 18:55:11 +0500

    up date

--- bot/app.py
+++ bot/app.py
@@ -42,7 +42,7 @@ def index():
 
 print(__name__)
 
-if __name__ == "bot.app":
+if __name__ == "__main__":
     bot.remove_webhook()
     bot.set_webhook(url=URL)
     app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)), debug=DEBUG_SWITCH)
