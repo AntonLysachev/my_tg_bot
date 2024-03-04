e287cd598f7f291f00671aa5843103a653362eb6
AntonLysachev <anton@lysachev.ru>
2024-03-04 17:28:16 +0500

    add config

--- bot/app.py
+++ bot/app.py
@@ -2,13 +2,15 @@ import os
 from flask import Flask, request
 import telebot
 from dotenv import load_dotenv
+import logging
+import config
 
 load_dotenv()
 
-TOKEN = os.getenv('TOKEN')
-URL = os.getenv('URL')
-bot = telebot.TeleBot(TOKEN)
+bot = telebot.TeleBot(config.BOT_TOKEN)
 app = Flask(__name__)
+logger = telebot.logger
+logger.setLevel(logging.DEBUG)
 
 
 @bot.message_handler(commands=['start'])
@@ -21,7 +23,7 @@ def echo_message(message):
     bot.send_message(message.chat.id, message.text)
 
 
-@app.route(f'/{TOKEN}/', methods=['POST'])
+@app.route(f'/{config.BOT_TOKEN}', methods=['POST'])
 def getMessage():
     json_string = request.get_data().decode('utf-8')
     update = telebot.types.Update.de_json(json_string)
@@ -36,5 +38,5 @@ def webhook():
 
 if __name__ == "__main__":
     bot.remove_webhook()
-    bot.set_webhook(url=URL)    
+    bot.set_webhook(url=config.APP_URL)    
     app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
